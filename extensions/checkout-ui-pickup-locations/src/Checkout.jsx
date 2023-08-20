a119e18c02621ffeb57c57f58c2e642ea73bb2bc
Hannah Robinson <hannah.robinson.git@gmail.com>
2023-08-20 15:43:52 +1200

    Add checkout ui extension boilerplate

---
+++ extensions/checkout-ui-pickup-locations/src/Checkout.jsx
@@ -0,0 +1,21 @@
+import {
+  Banner,
+  useApi,
+  useTranslate,
+  reactExtension,
+} from '@shopify/ui-extensions-react/checkout'
+
+export default reactExtension('purchase.checkout.block.render', () => (
+  <Extension />
+))
+
+function Extension() {
+  const translate = useTranslate()
+  const { extension } = useApi()
+
+  return (
+    <Banner title='checkout-ui-pickup-locations'>
+      {translate('welcome', { target: extension.target })}
+    </Banner>
+  )
+}
